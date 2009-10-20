2cc9252deda40f24a3aa6987c9b21bee3182d02b
Séverin Lemaignan <severin.lemaignan@laas.fr>
2009-10-20 11:10:53 +0200

    Display an error msg when the given path is invalid or unwritable + allow the specify a hostname and port

--- oro-apps-src/oro_export.cpp
+++ oro-apps-src/oro_export.cpp
@@ -17,33 +17,47 @@ using namespace oro;
 //Forward declarations
 void sigproc(int);
 
-const string hostname = "localhost";
-const string port = "6969";
+string hostname;
+string port;
 
 int main(int argc, char* argv[]) {
 
 	string path;
 	vector<string> result;
 	Ontology* onto;
+	
+	cout << "********* oro-export - Export the current ontology to a file *********" << endl;
+	if (argc != 3 && argc != 4){
+		cout << "Syntax:\n> oro-server host port [path]" << endl << "If no path is provided, the default one ('$(server_working_directory)/openrobots.owl') is used." << endl;
+		return(0);
+	}
 
 	//We catch ctrl+c to cleanly close the application
 	signal( SIGINT,sigproc);
 	
-	if (argc != 2){
+	hostname = argv[1];
+	port = argv[2];
+	
+	if (argc == 3){
 		cout << "Default path used. If you wish, you can provide a path + file name."<<endl;
 		path = "openrobots.owl";
 	}
-	else path = argv[1];
+	else path = argv[3];
 
 	
-	//Instanciate the ontology with the YARP connector.
+	//Instanciate the ontology with the socket connector.
 	SocketConnector connector(hostname, port);
 	onto = Ontology::createWithConnector(connector);
 
 	cout << "Exporting the current ontology to " << path << "... ";
 
-	
-	onto->save(path);
+	try {
+		onto->save(path);
+	} catch (OntologyServerException &e)
+	{
+		cerr << endl << "Error! Check the filename you provided is valid and points to a writable location for the server." << endl;
+		exit(1);
+	}
 
 	cout << "done." << endl;
 
