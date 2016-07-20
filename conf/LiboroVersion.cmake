8941bb7c0ece854e226378169da87f4375d3950c
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2016-07-20 10:49:31 +0100

    Clean-up CMakeLists

--- conf/LiboroVersion.cmake
+++ conf/LiboroVersion.cmake
@@ -1,7 +1,6 @@
 
 # This file is the official location of the current liboro version number.
-# Please do not add extra numerals in this file or change its formatting.
 
-SET(CPACK_PACKAGE_VERSION_MAJOR "1")
-SET(CPACK_PACKAGE_VERSION_MINOR "3")
-SET(CPACK_PACKAGE_VERSION_PATCH "1")
+set(CPACK_PACKAGE_VERSION_MAJOR "1")
+set(CPACK_PACKAGE_VERSION_MINOR "3")
+set(CPACK_PACKAGE_VERSION_PATCH "1")
